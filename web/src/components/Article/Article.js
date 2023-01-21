f5564f0612ae19b68fb447d7425ca2c1312bf7af
Wilkes Booth <andersonwilkesbooth@gmail.com>
2023-01-20 19:12:53 -0800

    article classes

--- web/src/components/Article/Article.js
+++ web/src/components/Article/Article.js
@@ -2,7 +2,7 @@ import { Link, routes } from '@redwoodjs/router'
 
 const Article = ({ article }) => {
   return (
-    <article key={article.id}>
+    <article id={`article-${article.id}`} className="article" key={article.id}>
       <header>
         <h2>
           <Link to={routes.article({ id: article.id })}>{article.title}</Link>
