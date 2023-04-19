0218e5c9e8b589e67a4af7b085a0fead23a2a443
Wilkes Booth <andersonwilkesbooth@gmail.com>
2023-04-18 21:31:58 -0700

    all sorts of stupid email shit

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,9 +1,66 @@
-import { MetaTags } from '@redwoodjs/web'
+import emailjs from '@emailjs/browser'
+
+import {
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+  Label,
+  FieldError,
+  useForm,
+  FormError,
+} from '@redwoodjs/forms'
+import { MetaTags, useMutation } from '@redwoodjs/web'
+import { Toaster, toast } from '@redwoodjs/web/toast'
+
+// import { sendEmail } from '../../../../api/src/lib/email'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 const AboutPage = () => {
+  const formMethods = useForm()
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Thank you for your message!')
+      formMethods.reset()
+    },
+  })
+
+  // const sendEmail = (data) => {
+  //   e.preventDefault();
+
+  //   emailjs.sendForm(process.env.EMAIL_JS_ID, form.current, )
+  //     .then((result) => {
+  //         console.log(result.text);
+  //     }, (error) => {
+  //         console.log(error.text);
+  //     });
+  // };
+
+  const onSubmit = (data) => {
+    // const subject = 'Your survey response was received!'
+    // const text = 'Thank you for giving your feedback.'
+    // const html = 'Thank you for giving your feedback.'
+    // sendEmail({ to: recipientEmail.trim(), subject, text, html })
+
+    console.log(data)
+
+    create({
+      variables: {
+        input: data,
+      },
+    })
+  }
+
   return (
     <>
-      <MetaTags title="About" description="About page" />
+      <MetaTags title="About/Contact" description="About/Contact Page" />
 
       <div className="about-page">
         <p>
@@ -19,6 +76,48 @@ const AboutPage = () => {
           film photography is just one of her other passions
         </p>
       </div>
+
+      <Toaster></Toaster>
+      <Form onSubmit={onSubmit} formMethods={formMethods} error={error}>
+        <FormError error={error} wrapperClassName="form-error" />
+        <Label errorClassName="error" name="name">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          errorClassName="error"
+          validation={{ required: true }}
+        />
+        <FieldError name="name" className="error" />
+
+        <Label errorClassName="error" name="email">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          errorClassName="error"
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+        />
+        <FieldError name="email" className="error" />
+
+        <Label errorClassName="error" name="message">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          errorClassName="error"
+          validation={{ required: true }}
+        />
+        <FieldError name="message" className="error" />
+
+        <Submit disabled={loading}>Send Message</Submit>
+      </Form>
     </>
   )
 }
